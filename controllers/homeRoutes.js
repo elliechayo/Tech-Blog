1676cdd5a72f663a1d1f79c3f7d0d051d4f8dcad
Elinor Chayo <elliechayo@gmail.com>
2023-05-22 22:52:33 -0400

    created the GET route to retrieves all posts and includes associated user information and render it to the homepage. Craeed the GET route to view  posts by id.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,3 +25,39 @@ router.get("/", async (req, res) => {
          res.render("homepage", { user: req.session.user, posts: [] });
      }
  });
+
+
+ // GET route for "/viewPost/:id"
+ router.get("/viewPost/:id", async (req, res) => {
+     const { id } = req.params;
+     try {
+           // Find the post with the specified ID and include associated user and comment information
+         let post = await Post.findOne({
+             where: { id },
+             include: [
+                 { model: User, attributes: ["username"] },
+                 {
+                     model: Comment,
+                     attributes: ["id", "comment", "postId", "createdBy", "createdAt"],
+                     include: {
+                         model: User,
+                         attributes: ["username"]
+                     }
+                 }
+             ]
+         });
+           // If the post doesn't exist, redirect to the homepage
+         if (!post) {
+             return res.redirect("/");
+         }
+         post = post.get({ plain: true });
+          // Render the viewPost page with the user data and the retrieved post
+         res.render("viewPost", { user: req.session.user, post: post });
+     } catch (error) {
+         console.error(error);
+           // If an error occurs, redirect to the homepage
+         return res.redirect("/");
+     }
+ });
+
+ module.exports = router;
