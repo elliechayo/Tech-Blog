a4cbf309f639886cde1708c3326249a17846a48f
Elinor Chayo <elliechayo@gmail.com>
2023-05-27 11:59:29 -0400

    added jawsdb configuration for heroku deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,27 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "127.0.0.1",
-    port: "3306",
-    dialect: "mysql"
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "127.0.0.1",
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
+  }
+
+// let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: "127.0.0.1",
+//     port: "3306",
+//     dialect: "mysql"
+// });
 
 module.exports = sequelize;
