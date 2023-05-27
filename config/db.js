9401e6a7eb6dfd806a5e83fe38abd27319069f9a
Elinor Chayo <elliechayo@gmail.com>
2023-05-27 17:33:32 -0400

    deleted the double const sequelize

--- config/db.js
+++ config/db.js
@@ -1,36 +1,25 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306,
-    });
 
-// const Sequelize = require("sequelize");
-// let sequelize;
+//const Sequelize = require("sequelize");
+let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//     sequelize = new Sequelize(process.env.JAWSDB_URL);
-//   } else {
-//     sequelize = new Sequelize(
-//       process.env.DB_NAME,
-//       process.env.DB_USER,
-//       process.env.DB_PASSWORD,
-//       {
-//         host: "127.0.0.1",
-//         dialect: "mysql",
-//         port: 3306,
-//       }
-//     );
-//   }
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "127.0.0.1",
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
+  }
 
-// let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: "127.0.0.1",
-//     port: "3306",
-//     dialect: "mysql"
-// });
+
 
 module.exports = sequelize;
